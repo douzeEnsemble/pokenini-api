95a9c18806aeba6abdc5e910c0d8c45297cdeddf
rlerognon <renaud.lerognon@fulll.fr>
2022-08-19 12:02:10 +0200

    Fix deploy script

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 php bin/console app:import:pokemon resources/data/pokemon_list.csv
 php bin/console app:import:game_availability resources/data/bulbapedia_availability.csv
 php bin/console app:calculate:game_bundle_availability
