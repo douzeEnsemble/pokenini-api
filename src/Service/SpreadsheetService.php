c73d16f7840cf9eb141773a88c48a3d45a579af7
rlerognon <renaud.lerognon@fulll.fr>
2022-09-21 17:29:45 +0200

    Remove dead code

--- src/Service/SpreadsheetService.php
+++ src/Service/SpreadsheetService.php
@@ -24,38 +24,17 @@ class SpreadsheetService
 
     public function get(string $spreadsheetId, string $range): ?ValueRange
     {
-        $key = base64_encode("$spreadsheetId-$range");
-
         return $this->service->spreadsheets_values->get($spreadsheetId, $range);
-
-//        /** @var ValueRange|null */
-//        return $this->cache?->get($key, function () use ($spreadsheetId, $range) {
-//            return $this->service->spreadsheets_values->get($spreadsheetId, $range);
-//        });
     }
 
     public function getSheetRowCount(string $spreadsheetId, string $sheetTitle): int
     {
-        $key = base64_encode("$spreadsheetId-$sheetTitle-rowCount");
-
         return $this->getSheetGridProperties($spreadsheetId, $sheetTitle)->getRowCount();
-
-//        /** @var int */
-//        return $this->cache?->get($key, function () use ($spreadsheetId, $sheetTitle) {
-//            return $this->getSheetGridProperties($spreadsheetId, $sheetTitle)->getRowCount();
-//        });
     }
 
     public function getSheetColumnCount(string $spreadsheetId, string $sheetTitle): int
     {
-        $key = base64_encode("$spreadsheetId-$sheetTitle-columnCount");
-
         return $this->getSheetGridProperties($spreadsheetId, $sheetTitle)->getColumnCount();
-
-//        /** @var int */
-//        return $this->cache?->get($key, function () use ($spreadsheetId, $sheetTitle) {
-//            return $this->getSheetGridProperties($spreadsheetId, $sheetTitle)->getColumnCount();
-//        });
     }
 
     private function getSheetGridProperties(string $spreadsheetId, string $sheetTitle): Sheets\GridProperties
