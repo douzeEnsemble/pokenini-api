44494878d42cd650365ca054780b6864c877667b
Renaud Douze <renauddouze@gmail.com>
2022-12-30 15:57:38 +0100

    Regional Dex Numbers use Region

---
+++ src/Repository/RegionRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\Region;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Region>
+ */
+class RegionRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Region::class);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getAllNames(): array
+    {
+        $queryBuilder = $this->createQueryBuilder('r');
+        $queryBuilder->select('r.name');
+        $queryBuilder->orderBy('r.orderNumber');
+
+        /** @var string[] */
+        return $queryBuilder->getQuery()->getSingleColumnResult();
+    }
+}
