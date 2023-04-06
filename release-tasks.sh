86e513e44aeea366ea0b402cc0eac4cd5b5e7ef9
Renaud Douze <renauddouze@gmail.com>
2023-04-06 08:38:18 +0200

    Remove Heroku config files

--- release-tasks.sh
+++
@@ -1,9 +0,0 @@
-php bin/console messenger:stop-workers
-
-php bin/console app:update:labels
-php bin/console app:update:games_and_dex
-php bin/console app:update:pokemons
-php bin/console app:update:regional_dex_numbers
-php bin/console app:update:games_availabilities
-php bin/console app:calculate:game_bundles_availabilities
-php bin/console app:calculate:dex_availabilities
