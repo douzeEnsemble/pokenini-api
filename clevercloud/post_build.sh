01ebdcbf86ba9791d15619a7880cf263f76f1f76
Renaud Douze <renauddouze@gmail.com>
2023-04-07 16:18:14 +0200

    Update post build scripts

--- clevercloud/post_build.sh
+++ clevercloud/post_build.sh
@@ -1,1 +1,14 @@
+./bin/console messenger:stop-workers
+
+echo ${GOOGLE_CREDENTIALS} > ./resources/auth/credentials.json
+chmod -R ugo+r ./resources/auth
+
 ./bin/console doctrine:migrations:migrate --no-interaction
+
+php bin/console app:update:labels
+php bin/console app:update:games_and_dex
+php bin/console app:update:pokemons
+php bin/console app:update:regional_dex_numbers
+php bin/console app:update:games_availabilities
+php bin/console app:calculate:game_bundles_availabilities
+php bin/console app:calculate:dex_availabilities
