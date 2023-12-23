d7d615022b1529939d55521807b7c3a5a79c0b57
Renaud Douze <renaud.douze@gmail.com>
2023-12-23 09:28:44 +0100

    Add test to get to 100% infection

--- tests/Functional/Repository/GamesAvailabilitiesRepositoryTest.php
+++ tests/Functional/Repository/GamesAvailabilitiesRepositoryTest.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional\Repository;
 
+use App\Entity\Game;
+use App\Entity\GameAvailability;
+use App\Entity\Pokemon;
 use App\Repository\GamesAvailabilitiesRepository;
+use App\Repository\GamesRepository;
+use App\Repository\PokemonsRepository;
 use App\Tests\Common\Traits\CounterTrait\CountGameAvailabilityTrait;
 use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
@@ -14,19 +19,74 @@ class GamesAvailabilitiesRepositoryTest extends KernelTestCase
     use RefreshDatabaseTrait;
     use CountGameAvailabilityTrait;
 
+    /** @var Game[] */
+    private array $games;
+    /** @var Pokemon[] */
+    private array $pokemons;
+
+    private GamesAvailabilitiesRepository $gamesAvailabilitiesRepo;
+    private GamesRepository $gamesRepo;
+    private PokemonsRepository $pokemonsRepo;
+
     public function setUp(): void
     {
         self::bootKernel();
+
+        // Using temp variables is for avoid typing conflict
+        /** @var GamesAvailabilitiesRepository $gamesAvailabilitiesRepo */
+        $gamesAvailabilitiesRepo = static::getContainer()->get(GamesAvailabilitiesRepository::class);
+        $this->gamesAvailabilitiesRepo = $gamesAvailabilitiesRepo;
+        /** @var GamesRepository $gamesRepo */
+        $gamesRepo = static::getContainer()->get(GamesRepository::class);
+        $this->gamesRepo = $gamesRepo;
+        /** @var PokemonsRepository $pokemonsRepo */
+        $pokemonsRepo = static::getContainer()->get(PokemonsRepository::class);
+        $this->pokemonsRepo = $pokemonsRepo;
     }
 
     public function testRemoveAll(): void
     {
         $this->assertGreaterThan(0, $this->getGameAvailabilityCount());
 
-        /** @var GamesAvailabilitiesRepository $repo */
-        $repo = static::getContainer()->get(GamesAvailabilitiesRepository::class);
-        $repo->removeAll();
+        $this->gamesAvailabilitiesRepo->removeAll();
 
         $this->assertEquals(0, $this->getGameAvailabilityCount());
+    }
+
+    public function testGetFromPokemon(): void
+    {
+        $pokemonDouze = $this->getPokemon('Douze');
+
+        $listDouze = $this->gamesAvailabilitiesRepo->getFromPokemon($pokemonDouze);
+        $this->assertNull($listDouze->nexistepas);
+        $this->assertTrue($listDouze->red);
+        $this->assertFalse($listDouze->green);
+        $this->assertTrue($listDouze->blue);
+        $this->assertFalse($listDouze->yellow);
+
+        $pokemonBulbasaur = $this->getPokemon('Bulbasaur');
+
+        $listBulbasaur = $this->gamesAvailabilitiesRepo->getFromPokemon($pokemonBulbasaur);
+        $this->assertNull($listBulbasaur->nexistepas);
+        $this->assertTrue($listBulbasaur->red);
+        $this->assertTrue($listBulbasaur->green);
+        $this->assertTrue($listBulbasaur->blue);
+        $this->assertTrue($listBulbasaur->yellow);
+    }
+
+    private function getPokemon(string $name): Pokemon
+    {
+        if (isset($this->pokemons[$name])) {
+            return $this->pokemons[$name];
+        }
+
+        /** @var Pokemon $pokemon */
+        $pokemon = $this->pokemonsRepo->findOneBy(['name' => $name]);
+
+        $this->assertNotNull($pokemon);
+
+        $this->pokemons[$name] = $pokemon;
+
+        return $pokemon;
     }
 }
--- tests/Functional/Repository/GamesShiniesAvailabilitiesRepositoryTest.php
+++ tests/Functional/Repository/GamesShiniesAvailabilitiesRepositoryTest.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional\Repository;
 
+use App\Entity\Game;
+use App\Entity\GameAvailability;
+use App\Entity\Pokemon;
 use App\Repository\GamesShiniesAvailabilitiesRepository;
+use App\Repository\GamesRepository;
+use App\Repository\PokemonsRepository;
 use App\Tests\Common\Traits\CounterTrait\CountGameShinyAvailabilityTrait;
 use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
@@ -14,19 +19,65 @@ class GamesShiniesAvailabilitiesRepositoryTest extends KernelTestCase
     use RefreshDatabaseTrait;
     use CountGameShinyAvailabilityTrait;
 
+    /** @var Game[] */
+    private array $games;
+    /** @var Pokemon[] */
+    private array $pokemons;
+
+    private GamesShiniesAvailabilitiesRepository $gamesShiniesAvailabilitiesRepo;
+    private GamesRepository $gamesRepo;
+    private PokemonsRepository $pokemonsRepo;
+
     public function setUp(): void
     {
         self::bootKernel();
+
+        // Using temp variables is for avoid typing conflict
+        /** @var GamesShiniesAvailabilitiesRepository $gamesShiniesAvailabilitiesRepo */
+        $gamesShiniesAvailabilitiesRepo = static::getContainer()->get(GamesShiniesAvailabilitiesRepository::class);
+        $this->gamesShiniesAvailabilitiesRepo = $gamesShiniesAvailabilitiesRepo;
+        /** @var GamesRepository $gamesRepo */
+        $gamesRepo = static::getContainer()->get(GamesRepository::class);
+        $this->gamesRepo = $gamesRepo;
+        /** @var PokemonsRepository $pokemonsRepo */
+        $pokemonsRepo = static::getContainer()->get(PokemonsRepository::class);
+        $this->pokemonsRepo = $pokemonsRepo;
     }
 
     public function testRemoveAll(): void
     {
         $this->assertGreaterThan(0, $this->getGameShinyAvailabilityCount());
 
-        /** @var GamesShiniesAvailabilitiesRepository $repo */
-        $repo = static::getContainer()->get(GamesShiniesAvailabilitiesRepository::class);
-        $repo->removeAll();
+        $this->gamesShiniesAvailabilitiesRepo->removeAll();
 
         $this->assertEquals(0, $this->getGameShinyAvailabilityCount());
+    }
+
+    public function testGetFromPokemon(): void
+    {
+        $pokemonDouze = $this->getPokemon('Douze');
+
+        $pokemonDeoxys = $this->getPokemon('Deoxys');
+
+        $listDeoxys = $this->gamesShiniesAvailabilitiesRepo->getFromPokemon($pokemonDeoxys);
+        $this->assertNull($listDeoxys->nexistepas);
+        $this->assertTrue($listDeoxys->ruby);
+        $this->assertFalse($listDeoxys->emerald);
+    }
+
+    private function getPokemon(string $name): Pokemon
+    {
+        if (isset($this->pokemons[$name])) {
+            return $this->pokemons[$name];
+        }
+
+        /** @var Pokemon $pokemon */
+        $pokemon = $this->pokemonsRepo->findOneBy(['name' => $name]);
+
+        $this->assertNotNull($pokemon);
+
+        $this->pokemons[$name] = $pokemon;
+
+        return $pokemon;
     }
 }
