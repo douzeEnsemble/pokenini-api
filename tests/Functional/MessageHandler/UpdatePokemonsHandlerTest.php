1feb5f5212783628d7d46256f5d53aa272e3fddb
Renaud Douze <renaud.douze@gmail.com>
2023-12-29 15:50:36 +0100

    Update Pokemons' spreadsheets

--- tests/Functional/MessageHandler/UpdatePokemonsHandlerTest.php
+++ tests/Functional/MessageHandler/UpdatePokemonsHandlerTest.php
@@ -49,7 +49,7 @@ class UpdatePokemonsHandlerTest extends KernelTestCase
 
         $transport->queue()->assertEmpty();
 
-        $this->assertEquals(1817, $this->getTableCount('pokemon'));
+        $this->assertEquals(1818, $this->getTableCount('pokemon'));
 
         $this->assertEquals($beforeTotalCount + 1, $this->getActionLogCount());
         $this->assertEquals($beforeToProcessCount, $this->getActionLogToProcessCount());
