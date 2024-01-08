95d5f147c2fd2ac1ae2d58e5b16602f45207b921
Renaud Douze <renaud.douze@gmail.com>
2024-01-08 11:58:16 +0100

    Update to SF 7.0

--- src/Entity/Traits/BaseEntityTrait.php
+++ src/Entity/Traits/BaseEntityTrait.php
@@ -6,10 +6,13 @@ namespace App\Entity\Traits;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Uid\Uuid;
+use Symfony\Bridge\Doctrine\Types\UuidType;
 
 trait BaseEntityTrait
 {
-    #[ORM\Id, ORM\Column(name: 'id', type: 'uuid', unique: true), ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\Id]
+    #[ORM\Column(name: 'id', type: UuidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
     private ?Uuid $identifier = null;
 
