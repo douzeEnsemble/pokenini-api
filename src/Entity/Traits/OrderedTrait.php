249db1e45f488c42ff7ffb4a48a3b634c291c205
Renaud Douze <renauddouze@gmail.com>
2024-02-05 16:03:05 +0100

    Clean use

--- src/Entity/Traits/OrderedTrait.php
+++ src/Entity/Traits/OrderedTrait.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Entity\Traits;
 
-use Symfony\Component\Serializer\Annotation\Groups;
-use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 
 trait OrderedTrait
