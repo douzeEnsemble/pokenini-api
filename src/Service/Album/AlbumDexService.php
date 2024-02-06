9a32aab822130f50d8b78c25088b9e461c3cddfe
Renaud Douze <renauddouze@gmail.com>
2024-02-06 12:12:50 +0100

    Add AlbumDexService

---
+++ src/Service/Album/AlbumDexService.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Album;
+
+use App\Repository\DexRepository;
+
+class AlbumDexService
+{
+    public function __construct(
+        private readonly DexRepository $dexRepository,
+    ) {
+    }
+
+    /**
+     * @return string[]|bool[]
+     */
+    public function getData(string $trainerExternalId, string $dexSlug): array
+    {
+        return $this->dexRepository->getData($trainerExternalId, $dexSlug);
+    }
+}
---
+++ tests/Functional/Service/Album/AlbumDexServiceTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Service\Album;
+
+use App\Service\Album\AlbumDexService;
+use App\Tests\Common\Traits\CounterTrait\CountGameBundleAvailabilityTrait;
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class AlbumDexServiceTest extends KernelTestCase
+{
+    use RefreshDatabaseTrait;
+    use CountGameBundleAvailabilityTrait;
+
+    public function setUp(): void
+    {
+        self::bootKernel();
+    }
+
+    public function testGetData(): void
+    {
+        /** @var AlbumDexService $service */
+        $service = static::getContainer()->get(AlbumDexService::class);
+
+        $dexRGBY = $service->getData('7b52009b64fd0a2a49e6d8a939753077792b0554', 'redgreenblueyellow');
+
+        $this->assertArrayHasKey('name', $dexRGBY);
+        $this->assertEquals('Red / Green / Blue / Yellow', $dexRGBY['name']);
+        $this->assertArrayHasKey('selection_rule', $dexRGBY);
+        $this->assertEquals(
+            '(p.bankable or p.bankableish) and ba?.redgreenblueyellow',
+            $dexRGBY['selection_rule']
+        );
+        $this->assertArrayHasKey('is_private', $dexRGBY);
+        $this->assertFalse($dexRGBY['is_private']);
+
+        $dexGSC = $service->getData('7b52009b64fd0a2a49e6d8a939753077792b0554', 'goldsilvercrystal');
+
+        $this->assertArrayHasKey('name', $dexGSC);
+        $this->assertEquals('Gold / Silver / Crystal', $dexGSC['name']);
+        $this->assertArrayHasKey('selection_rule', $dexGSC);
+        $this->assertEquals(
+            '(p.bankable or p.bankableish) and ba?.goldsilvercrystal '
+            . 'and p.specialForm === null and p.regionalForm === null',
+            $dexGSC['selection_rule']
+        );
+        $this->assertArrayHasKey('is_private', $dexGSC);
+        $this->assertTrue($dexGSC['is_private']);
+
+        $this->assertEmpty($service->getData('7b52009b64fd0a2a49e6d8a939753077792b0554', 'dexthatdoesntexists'));
+    }
+}
