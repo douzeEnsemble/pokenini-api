f6c83cf3add5611f27ba64bffb6dc6701c51a4e9
Renaud Douze <renauddouze@gmail.com>
2024-02-06 12:40:50 +0100

    Add AlbumReportService

---
+++ src/Service/Album/AlbumReportService.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Album;
+
+use App\DTO\AlbumReport\Report;
+use App\DTO\AlbumReport\Statistic;
+use App\Repository\DexAvailabilitiesRepository;
+use App\Repository\PokedexRepository;
+
+class AlbumReportService
+{
+    public function __construct(
+        private readonly PokedexRepository $pokedexRepository,
+        private readonly DexAvailabilitiesRepository $dexAvailabilitiesRepository,
+    ) {
+    }
+
+    public function getReport(string $trainerExternalId, string $dexSlug): Report
+    {
+        $totalCaught = 0;
+        $detail = [];
+
+        $total = $this->dexAvailabilitiesRepository->getTotal($dexSlug);
+        $totalUncaught = $total;
+
+        $catchStatesCounts = $this->pokedexRepository->getCatchStatesCounts($trainerExternalId, $dexSlug);
+        foreach ($catchStatesCounts as $catchStatesCount) {
+            $detail[] = new Statistic(
+                (string) $catchStatesCount['slug'],
+                (string) $catchStatesCount['name'],
+                (string) $catchStatesCount['french_name'],
+                (int) $catchStatesCount['count'],
+            );
+
+            if ('yes' === $catchStatesCount['slug']) {
+                $totalCaught = (int) $catchStatesCount['count'];
+            }
+
+            if ('no' !== $catchStatesCount['slug']) {
+                $totalUncaught -= $catchStatesCount['count'];
+            }
+        }
+
+        return new Report($total, $totalCaught, $totalUncaught, $detail);
+    }
+}
