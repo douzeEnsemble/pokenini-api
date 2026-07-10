0729b33cb3eb2389e455754dd36d1311580297e2
Renaud Douze <renaud.douze@gmail.com>
2026-07-10 07:12:19 +0200

    Fix CS

--- src/DTO/ElectionReport/Report.php
+++ src/DTO/ElectionReport/Report.php
@@ -7,7 +7,7 @@ namespace App\DTO\ElectionReport;
 final class Report
 {
     /**
-     * @param array<array-key, array<string, mixed>>                                                                                              $top
+     * @param array<array-key, array<string, mixed>>                                                                                                                        $top
      * @param array{view_count_sum: int, win_count_sum: int, view_count_max: int, win_count_max: int, under_max_view_count: int, max_view_count: int, dex_total_count: int} $metrics
      */
     public function __construct(
