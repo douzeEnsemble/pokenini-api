7a36cb0fa19436f63fae43ab89435ea3606661df
Renaud Douze <renaud.douze@gmail.com>
2026-08-16 22:37:33 +0200

    feat: add nullable dex.banner_layers column

---
+++ migrations/2026/08/Version20260816203715.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20260816203715 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE dex ADD banner_layers TEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE dex DROP banner_layers');
+    }
+}
